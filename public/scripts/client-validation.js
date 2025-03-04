e1b99a3eb92fd3c44caef5290e6a58f1f59c6867
Dani-DEV28 <193554832+Dani-DEV28@users.noreply.github.com>
2025-03-04 14:53:42 -0800

    validate, and change file direct

---
+++ public/scripts/client-validation.js
@@ -0,0 +1,41 @@
+document.getElementById('blog-form').onsubmit = () => {
+
+    clearErrors();
+
+    let isValid = true;
+
+    // Validate first name
+    let fname = document.getElementById('name').value.trim();
+    if (fname === "" || containsNumbers(fname)) {
+        document.getElementById("err-name").style.display = "block";
+        isValid = false;
+    }
+
+    // Validate last name
+    let title = document.getElementById('title').value.trim();
+    if (title === "") {
+        document.getElementById("err-title").style.display = "block";
+        isValid = false;
+    }
+
+    // Validate last name
+    let content = document.getElementById('content').value.trim();
+    if (content === "" || content.lenght <= 10) {
+        document.getElementById("err-content").style.display = "block";
+        isValid = false;
+    }
+
+    return isValid;
+}
+
+function clearErrors() {
+    let errors = document.getElementsByClassName("err");
+    for (let i = 0; i < errors.length; i++) {
+        errors[i].style.display = "none";
+    }
+}
+
+function containsNumbers(str) {
+    return /\d/.test(str);
+  }
+  
